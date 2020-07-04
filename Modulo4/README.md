d8039f5d780a2a3f6bc16faa368f22aba960e098
Felipe Eduardo Gomes <felipe.edug@gmail.com>
2020-07-04 14:30:04 -0300

    trabalho final

---
+++ Modulo4/README.md
@@ -0,0 +1,14 @@
+<p align="center">
+  <img src="/assets/bootcamp_fullstack.png">
+</p>
+<h1 align="center">Bootcamp-nline-Desenvolvedor-Full-Stack - Maio 2020 (IGTI)</h1>
+
+# Persistência de dados, versionamento de código e implantação
+
+## Aulas
+- [Aula 2.2 - Integração do MongoDB Atlas](Aula2_2)
+- [Aula 2.3 - Mongoose](Aula2_3)
+- [Aula 2.4 - API CRUD com Mongoose](Aula2_4)
+
+## Trabalho
+- [Trabalho Prático Módulo 4](Trabalho4)
---
+++ Modulo4/Trabalho4/my-bank-api/routes/consultaRoute.js
@@ -0,0 +1,37 @@
+import express from 'express';
+
+import { accountModel } from '../models/account.js';
+
+const router = express.Router();
+
+/*
+Item 6: Crie  um  endpoint  para  consultar  o  saldo  da  conta.  
+Este  endpoint  deverá  receber como parâmetro a “agência” e o
+número da “conta”,e deverá retornar seu “balance”. Caso a conta 
+informada não exista, retornar um erro 
+
+Utilização:
+enviar um get para http:localhost:3000/consulta?agencia=10&conta=1001
+*/
+
+router.get('/', async (req, res) => {
+  try {
+    const { agencia, conta } = req.query;
+
+    const account = await accountModel.findOne({ agencia, conta });
+    //const account = await accountModel.find({});
+
+    if (!account) {
+      res.status(400).send('Conta não encontrada!');
+    } else {
+      res.send({
+        name: account.name,
+        balance: account.balance,
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+export default router;
